e10a509427c9d84a3f136298f12138f821659342
Manuel Gomes <manuel.fgj@gmail.com>
2020-04-27 13:10:23 -0300

    Category

---
+++ src/main/java/com/manuelfgj/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.manuelfgj.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.manuelfgj.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}

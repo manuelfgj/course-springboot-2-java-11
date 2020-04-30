486f4f033550b6ab760bc2933c5a1c909af52d2c
Manuel Gomes <manuel.fgj@gmail.com>
2020-04-30 11:58:23 -0300

    Exception handling - delete

--- src/main/java/com/manuelfgj/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/manuelfgj/course/resources/exceptions/ResourceExceptionHandler.java
@@ -2,7 +2,6 @@ package com.manuelfgj.course.resources.exceptions;
 
 import java.time.Instant;
 
-import javax.net.ssl.SSLEngineResult.Status;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
@@ -10,14 +9,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.manuelfgj.course.services.exceptions.DatabaseException;
 import com.manuelfgj.course.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
+	
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandardError> resouceNotFound(ResourceNotFoundException e, HttpServletRequest request){
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);		
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);		
 	}
